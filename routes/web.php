8720490ae1ac8ad3b1f496af2990f53116b32d8e
Arnav <arnchkv@gmail.com>
2025-08-08 12:20:03 +0530

    event, demo page created.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\DemoController;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('demo');
+// });
+
+Route::get("/", [DemoController::class, "index"])->name("home");
+
+Broadcast::routes([''=> '']);
